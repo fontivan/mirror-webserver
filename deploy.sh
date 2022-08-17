5a33c4c0dce26df99d5ca2be9145c2d783385444
Steven Skeard <sskeard@redhat.com>
2022-08-17 15:45:23 -0400

    Add query loop to deploy script to check for build status

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 
+function loop() {
+
+    local max_attempts
+    max_attempts=10
+
+    local interval
+    interval=30
+
+    local current_attempt
+    current_attempt=0
+
+    while [[ $max_attempts -gt $current_attempt ]] ;
+    do
+        sleep $INTERVAL
+        if is_build_success;
+        then
+            return 1
+        fi
+        current_attempt=$((current_attempt+1))
+    done
+
+    return 0
+}
+
+function is_build_success() {
+
+    local status
+    status=$(oc describe build mirror-webserver | grep 'Status' | awk '{print $2}')
+    if [[ "${status}" == "Success" ]]
+    then
+        return 1
+    fi
+    return 0
+}
+
+
 if [[ -z "${KUBECONFIG}" ]];
 then    
     echo "KUBECONFIG not defined."
@@ -10,7 +46,11 @@ fi
 oc apply -f src/buildconfig.yaml
 
 # Wait for build to finish
-echo 'tbd'
+if ! loop;
+then
+    echo 'Build did not succeed in maximum time allowed.'
+    return 1
+fi
 
 # Upload deploy config
 oc apply -f src/deployment.yaml
